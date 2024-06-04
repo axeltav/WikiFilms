891b8e58ca548f68961891f61059c6cfdd2f1095
ahmed ouannane <ghostmarktrick@gmail.com>
2024-06-04 15:35:50 +0200

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
   return (
     <>
-      
-      <p>Navbar</p>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <Link className="navbar-brand" href="#">Wikifilms</Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <Link className="nav-link" href="/">Accueil<span className="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="/">Film</Link>
+      </li>
+    </ul>
+  </div>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+</nav>
     </>
   );
 }
