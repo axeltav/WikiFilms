eb2d07d8b88d46060ef52ffbfc6fb136ed758093
Axel Tavernet <axeltavernet@gmail.com>
2024-06-06 16:55:44 +0200

    add filters and note in films list

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,6 +15,12 @@ export const MovieCard = ({movie = {}}) => {
         <div  className="col-2 pb-3 mx-3" >
             <div className="card poster" onClick={() => routeChangeFilm(movie.id)}>
                 <img className="card-img-top " src={"https://image.tmdb.org/t/p/w200"+ movie.poster_path} alt="Card image cap"/>
+                <div className="overley-note d-flex justify-content-center align-items-center"
+                  style={Math.round(movie.vote_average*10) >=70 ? {borderColor: "green"} : 
+                  Math.round(movie.vote_average*10) >=40 ?{borderColor: "yellow"} : 
+                  movie.vote_count > 0 ?{borderColor: "red"} : {}}>
+                  <p className="overley-text mb-0">{movie.vote_count > 0 ? Math.round(movie.vote_average*10) : 'NR' }</p>
+                </div>
                 <div className="card-body pt-2">
                 <h5 className="card-title poster-title mb-1">{movie.title}</h5>
                 <p className="card-text poster-date">{movie.release_date}</p>
--- src/view/Accueil.jsx
+++ src/view/Accueil.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useAxiosOpti } from "../hooks/useAxiosOpti";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { MovieList } from "../components/MovieList";
+import RangeSlider from "../components/RangeSlider";
 
 export const Accueil = () => {
 
@@ -9,20 +10,86 @@ export const Accueil = () => {
     const [hasMore, setHasMore] = useState(true);
     const [page, setPage] = useState(1);
 
+    const [genres, setGenres] = useState([]);
+    const [genresApply, setgenresApply] = useState([]);
+    const [genresChecked, setGenresChecked] = useState([]);
+
+    const [startRealeaseDateApply, setStartRealeaseDateApply] = useState('');
+    const [startRealeaseDateSelected, setStartRealeaseDateSelected] = useState('');
+    const [endRealeaseDateApply, setEndRealeaseDateApply] = useState('');
+    const [endRealeaseDateSelected, setEndRealeaseDateSelected] = useState('');
+
+    const [toTimeApply, setToTimeApply] = useState(400);
+    const [toTimeSelected, setToTimeSelected] = useState(400);
+    const [fromTimeApply, setFromTimeApply] = useState(0);
+    const [fromTimeSelected, setFromTimeSelected] = useState(0);
+    
+    const [toNoteApply, setToNoteApply] = useState(400);
+    const [toNoteSelected, setToNoteSelected] = useState(400);
+    const [fromNoteApply, setFromNoteApply] = useState(0);
+    const [fromNoteSelected, setFromNoteSelected] = useState(0);
+
     const {get} = useAxiosOpti();
 
     useEffect(() => {
-        console.log('requete en cours');
-        
-        get(`discover/movie?page=${page}&api_key=de399415d2204316dcf46dabb3632ce6`)
+
+      let params = '';
+
+      if(toTimeApply && toTimeApply < 400){
+        params = params + `&with_runtime.lte=${toTimeApply}`;
+      }
+
+      if(fromTimeApply){
+        params = params + `&with_runtime.gte=${fromTimeApply}`;
+      }
+
+        get(`discover/movie?page=${page}&language=fr&api_key=de399415d2204316dcf46dabb3632ce6` + params)
             .then(res => {
-              setPage(currentVal => currentVal + 1);
+              setPage(2);
               setMovies(res.data.results);
+            }) 
+
+        get(`genre/movie/list?language=fr&api_key=de399415d2204316dcf46dabb3632ce6`)
+            .then(res => {
+              setGenres(res.data.genres);
             }) 
     },[]);
 
     const fetchMoreMovies = () => {
-      get(`discover/movie?page=${page}&api_key=de399415d2204316dcf46dabb3632ce6`)
+
+      let params = '';
+
+      if (genresApply.length > 0) {
+        params = `&with_genres=${genresApply.join('%7C%7C')}`;
+      }
+
+      if(startRealeaseDateApply){
+        const date = new Date(startRealeaseDateApply);
+        params = params + `&primary_release_date.gte=${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+      }
+      
+      if(endRealeaseDateApply){
+        const date = new Date(endRealeaseDateApply);
+        params = params + `&primary_release_date.lte=${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+      }
+
+      if(toTimeApply && toTimeApply < 400){
+        params = params + `&with_runtime.lte=${toTimeApply}`;
+      }
+
+      if(fromTimeApply){
+        params = params + `&with_runtime.gte=${fromTimeApply}`;
+      }
+
+      if(toNoteApply) {
+        params = params + `&vote_average.lte=${toNoteSelected}`;
+      }
+
+      if(fromNoteApply) {
+        params = params + `&vote_average.gte=${fromNoteSelected}`;
+      }
+
+      get(`discover/movie?page=${page}&language=fr&api_key=de399415d2204316dcf46dabb3632ce6` + params)
             .then(res => {
               setPage(currentVal => currentVal + 1);
               setMovies(currentVal => currentVal.concat(res.data.results));
@@ -30,23 +97,136 @@ export const Accueil = () => {
             })
     }
 
+    const changeGenresSelected = (e) => {
+      if (e.target.checked) {
+        setGenresChecked([...genresChecked, e.target.id]);
+      } else {
+        setGenresChecked(genresChecked.filter(genre => genre!== e.target.id));
+      }
+    }
+
+    const changeStartRealeaseDateSelected = (e) => {
+      setStartRealeaseDateSelected(e.target.value);
+    }
+
+    const changeEndRealeaseDateSelected = (e) => {
+      setEndRealeaseDateSelected(e.target.value);
+    }
+
+    const validFilters = (e) => {
+      e.preventDefault();
+
+      let params = '';
+
+      if (genresChecked.length > 0) {
+        params = `&with_genres=${genresChecked.join('%7C%7C')}`;
+      }
+
+      if(startRealeaseDateSelected){
+        const date = new Date(startRealeaseDateSelected);
+        params = params + `&primary_release_date.gte=${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+      }
+      
+      if(endRealeaseDateSelected){
+        const date = new Date(endRealeaseDateSelected);
+        params = params + `&primary_release_date.lte=${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+      }
+
+      if(toTimeSelected && toTimeSelected < 400) {
+        params = params + `&with_runtime.lte=${toTimeSelected}`;
+      }
+
+      if(fromTimeSelected) {
+        params = params + `&with_runtime.gte=${fromTimeSelected}`;
+      }
+
+      if(toNoteSelected) {
+        params = params + `&vote_average.lte=${toNoteSelected}`;
+      }
+
+      if(fromNoteSelected) {
+        params = params + `&vote_average.gte=${fromNoteSelected}`;
+      }
+
+      setgenresApply(genresChecked);
+
+      setStartRealeaseDateApply(startRealeaseDateSelected);
+      setEndRealeaseDateApply(endRealeaseDateSelected);
+
+      setToTimeApply(toTimeSelected);
+      setFromTimeApply(fromTimeSelected);
+      
+      setToNoteApply(toNoteSelected);
+      setFromNoteApply(fromNoteSelected);
+
+      get(`discover/movie?page=1&language=fr&api_key=de399415d2204316dcf46dabb3632ce6` + params)
+            .then(res => {
+              setPage(2);
+              setMovies(res.data.results);
+              res.data.results.length >= 20 ? setHasMore(true) : setHasMore(false);
+            })
+    }
+
+
+    
     
 
   return (
     <>
-      <h1>Liste des films</h1>
       <InfiniteScroll
-        className=""
         dataLength={movies.length}
         next={fetchMoreMovies}
         hasMore={hasMore}
         loader={<p style={{color: "white"}}>Chargement de nouveaux films ...</p>}>
       <div className="container">
         <div className="row" style={{display:"flex", flexWrap: "wrap"}}>
-          <div className="col-3">
+          <div className="col-3 d-flex flex-column align-items-center pt-5 filters">
+            <h1>Liste des films</h1>
             {/* filtres ici */}
+            <p>Filtres</p>
+
+            <form action="" onSubmit={validFilters} className="formFilter">
+              <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Séléctionez les genres
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  {genres.map(genre => {
+                    return (
+                      <div key={genre.id} className="form-check">
+                        <input className="form-check-input" type="checkbox" value="" id={genre.id} onChange={changeGenresSelected}/>
+                        <label className="form-check-label" htmlFor={genre.id} >
+                          {genre.name}
+                        </label>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+              <div className="form-group pt-3">
+                <p>Dates de sorties :</p>
+                <div className="d-flex justify-content-between pb-3">
+                  <label htmlFor="startRealeaseDate" className="white">Du</label>
+                  <input type="date" className="form-control dateInput" id="startRealeaseDate" onChange={changeStartRealeaseDateSelected}/>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <label htmlFor="endRealeaseDate" className="white">Au</label>
+                  <input type="date" className="form-control dateInput" id="endRealeaseDate" onChange={changeEndRealeaseDateSelected}/>
+                </div>
+              </div>
+              <div>
+                <p>Durée du film :</p>
+                <RangeSlider updateTo={setToTimeSelected} updateFrom={setFromTimeSelected} max={400} name="runTime"/>
+
+                <p>évaluation des utilisateurs :</p>
+                <RangeSlider updateTo={setToNoteSelected} updateFrom={setFromNoteSelected} max={10} name="note"/>
+              </div>
+              <div className="validation">
+                <button type="submit" className="btn validBtn">Valider</button>
+              </div>
+            </form>
           </div>
-          <div className="col-9 px-0">
+          <div className="col-9 pt-5 px-0">
             <MovieList movies={movies}/>
           </div>
         </div>
@@ -56,3 +236,6 @@ export const Accueil = () => {
     </>
   );
 }
+
+
+
